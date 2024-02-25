ec5536354e75a43bdde87daac89913749c651ad0
Ritammishra <mishraritam935@gamil.com>
2024-02-25 22:44:03 +0530

    Fetch api

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,11 +1,21 @@
-import React from "react";
+import axios  from "axios";
+import React, { useEffect, useState } from "react";
 
 export default function Products(){
+    const[products, setProducts] = useState();
+
+    useEffect(() => {
+        axios.get('https://api.escuelajs.co/api/v1/products')
+        .then(
+            (res)=>{
+                console.log(res.data);
+            }
+        )
+        .catch((err)=>console.log(err))
+    },[])
 
     return(
-
-        <>
-        <h3>Products</h3>
-        </>
+       <>
+       </>
     )
 } 
