e09d92563efe1aaac3f5214cd75c0b591034a519
Ritammishra <mishraritam935@gamil.com>
2024-02-25 22:59:51 +0530

    update

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -2,13 +2,14 @@ import axios  from "axios";
 import React, { useEffect, useState } from "react";
 
 export default function Products(){
-    const[products, setProducts] = useState();
+    const[products, setProducts] = useState([]);
 
     useEffect(() => {
         axios.get('https://api.escuelajs.co/api/v1/products')
         .then(
             (res)=>{
                 console.log(res.data);
+                setProducts([...products, ...res.data])
             }
         )
         .catch((err)=>console.log(err))
@@ -16,6 +17,16 @@ export default function Products(){
 
     return(
        <>
+       <h3>Products</h3>
+       <ul>
+                {products.map(product => (
+                    <li key={product.id}>
+                        <h4>{product.name}</h4>
+                        <p>Price: ${product.price}</p>
+                        <p>{product.description}</p>
+                    </li>
+                ))}
+            </ul>
        </>
     )
 } 
